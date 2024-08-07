81819406c164894be1787950195dd2b0227c0c2e
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-08-07 14:19:15 -0300

    modal interception...

--- src/app/components/modals/ModalInterception.tsx
+++ src/app/components/modals/ModalInterception.tsx
@@ -2,7 +2,11 @@
 import { useCallback, useRef, useEffect, MouseEventHandler } from "react";
 import { useRouter } from "next/navigation";
 
-export default function ModalInterception({ children }: { children: React.ReactNode }) {
+export default function ModalInterception({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const overlay = useRef<HTMLDivElement>(null);
   const wrapper = useRef<HTMLDivElement>(null);
   const router = useRouter();
@@ -35,12 +39,12 @@ export default function ModalInterception({ children }: { children: React.ReactN
   return (
     <div
       ref={overlay}
-      className="fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/60 p-10"
+      className="fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/60 overflow-y-auto p-0 md:p-10 backdrop-blur-sm"
       onClick={onClick}
     >
       <div
         ref={wrapper}
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:w-10/12 md:w-8/12 lg:w-2/5 p-6"
+        className="absolute  md:top-1/2 left-1/2 -translate-x-1/2 md:-translate-y-1/2 w-[100vw]  md:h-auto md:w-8/12 lg:w-2/5 p-6"
       >
         {children}
       </div>
