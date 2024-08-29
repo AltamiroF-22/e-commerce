fcce6baa460b739811635cb264e016ed96725ec7
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-08-28 22:44:49 -0300

    wallet navbar

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Links from "./link";
 import Logo from "./Logo";
 import MenuOptions from "./MenuOptions";
@@ -8,6 +8,8 @@ import { SafeUser } from "@/app/types";
 import { RiShoppingBagLine } from "react-icons/ri";
 import useCartModal from "@/app/hooks/useCartModal";
 import { useCart } from "@/app/context/CartContex";
+import { usePathname } from "next/navigation";
+import { AiOutlineWallet } from "react-icons/ai";
 
 interface NavbarProps {
   currentUser: SafeUser | null;
@@ -16,6 +18,7 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   const [navBorder, setNavBorder] = useState<boolean>(false);
   const cartmodal = useCartModal();
+  const pathname = usePathname();
 
   // não ta att auto
   const { cartItems } = useCart();
@@ -63,28 +66,38 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
         <Logo bigLogo />
 
         <div className="flex items-center gap-5">
-          <button
-            className="relative"
-            onClick={() => {
-              currentUser && handleToggleCartModal();
-            }}
-          >
-            {cartItems.length > 0 && (
-              <div className="h-4 w-4 rounded-full top-[-8px] right-[-8px] absolute bg-black flex items-center justify-center">
-                <p className="text-[10px] text-white">{cartItems.length}</p>
-              </div>
-            )}
+          {pathname !== "/checkout" && (
+            <button
+              className={`relative`}
+              onClick={() => {
+                currentUser && handleToggleCartModal();
+              }}
+            >
+              {cartItems.length > 0 && (
+                <div className="h-4 w-4 rounded-full top-[-8px] right-[-8px] absolute bg-black flex items-center justify-center">
+                  <p className="text-[10px] text-white">{cartItems.length}</p>
+                </div>
+              )}
 
-            <p className="sr-only">cart</p>
-            <RiShoppingBagLine
-              className={`text-zinc-800 text-2xl hover:opacity-85 transition ${
-                currentUser
-                  ? ""
-                  : "opacity-45 hover:opacity-45 cursor-not-allowed"
-              }`}
-            />
-          </button>
-          
+              <p className="sr-only">cart</p>
+              <RiShoppingBagLine
+                className={`text-zinc-800 text-2xl hover:opacity-85 transition ${
+                  currentUser
+                    ? ""
+                    : "opacity-45 hover:opacity-45 cursor-not-allowed"
+                }`}
+              />
+            </button>
+          )}
+
+          {pathname === "/checkout" && (
+            <button className="flex gap-2 items-center justify-center">
+              <p className="sr-only">wallet</p>
+              <AiOutlineWallet className="text-zinc-800 text-2xl hover:opacity-85 transition" />
+              <span>${currentUser?.wallet.toFixed(2)}</span>
+            </button>
+          )}
+
           <MenuOptions currentUser={currentUser} />
         </div>
       </div>
