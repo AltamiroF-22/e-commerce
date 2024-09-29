b295deb24b57b12abd9562efe60a3c85bec5b23d
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-09-29 00:11:30 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/app/api/user/currentUser/route.ts
+++ src/app/api/user/currentUser/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"; // Importa o NextResponse
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import prisma from "@/app/libs/prismadb";
 
-export async function getSession() {
+async function getSession() {
   return await getServerSession(authOptions);
 }
 
@@ -35,3 +35,44 @@ export async function GET() {
     return NextResponse.json({ error: "Something went wrong" }, { status: 500 }); // Erro genérico de servidor
   }
 }
+
+
+
+
+// import { getServerSession } from "next-auth/next";
+// import { NextResponse } from "next/server"; // Importa o NextResponse
+// import { authOptions } from "@/pages/api/auth/[...nextauth]";
+// import prisma from "@/app/libs/prismadb";
+
+// export async function getSession() {
+//   return await getServerSession(authOptions);
+// }
+
+// export async function GET() {
+//   try {
+//     const session = await getSession();
+
+//     if (!session?.user?.email) {
+//       return NextResponse.json({ message: "No user session found" }, { status: 401 }); // Retorna erro de autenticação
+//     }
+
+//     const currentUser = await prisma.eComerceUser.findUnique({
+//       where: {
+//         email: session.user.email as string,
+//       },
+//     });
+
+//     if (!currentUser) {
+//       return NextResponse.json({ message: "User not found" }, { status: 404 }); // Usuário não encontrado
+//     }
+
+//     return NextResponse.json({
+//       ...currentUser,
+//       createdAt: currentUser.createdAt.toISOString(),
+//       updatedAt: currentUser.updatedAt.toISOString(),
+//     });
+//   } catch (err: any) {
+//     console.log(err);
+//     return NextResponse.json({ error: "Something went wrong" }, { status: 500 }); // Erro genérico de servidor
+//   }
+// }
